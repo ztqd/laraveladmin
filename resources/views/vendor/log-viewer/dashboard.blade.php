b695006d465e5f34c7441fbf33b3bcc051daeef8
amenging <1508235075@qq.com>
2017-11-23 12:22:36 +0800

    dashboard

--- resources/views/vendor/log-viewer/dashboard.blade.php
+++ resources/views/vendor/log-viewer/dashboard.blade.php
@@ -79,7 +79,7 @@
         color: #fff;font-size: 3em;
     }*/
 </style>
-{{-- <div id="allmap" style="width:100%;overflow:hidden;margin:0;z-index:1000"></div> --}}
+<div id="allmap" style="width:100%;overflow:hidden;margin:0;z-index:1000"></div>
 <section class="box-body"> {{-- hidden-xs --}}
 {{-- <ul class="page_ul" style="margin: auto;">
     <li class="col-sm-6">
@@ -267,80 +267,80 @@
             var data = {!! $reports !!};
             console.log(data)
 
-            // var icon1 = new BMap.Icon('/img/star1.png', new BMap.Size(20, 35), {
-            //     imageSize: new BMap.Size(20, 30)
-            // })
-            // var icon2 = new BMap.Icon('/img/star2.png', new BMap.Size(20, 35), {
-            //     imageSize: new BMap.Size(20, 30)
-            // })
+            var icon1 = new BMap.Icon('/img/star1.png', new BMap.Size(20, 35), {
+                imageSize: new BMap.Size(20, 30)
+            })
+            var icon2 = new BMap.Icon('/img/star2.png', new BMap.Size(20, 35), {
+                imageSize: new BMap.Size(20, 30)
+            })
 
-            // var icon3 = new BMap.Icon('/img/star3.png', new BMap.Size(20, 35), {
-            //     imageSize: new BMap.Size(20, 30)
-            // })
-            // var icon4 = new BMap.Icon('/img/star4.png', new BMap.Size(20, 35), {
-            //     imageSize: new BMap.Size(20, 30)
-            // })
-            // var icon5 = new BMap.Icon('/img/star5.png', new BMap.Size(20, 35), {
-            //     imageSize: new BMap.Size(20, 30)
-            // })
+            var icon3 = new BMap.Icon('/img/star3.png', new BMap.Size(20, 35), {
+                imageSize: new BMap.Size(20, 30)
+            })
+            var icon4 = new BMap.Icon('/img/star4.png', new BMap.Size(20, 35), {
+                imageSize: new BMap.Size(20, 30)
+            })
+            var icon5 = new BMap.Icon('/img/star5.png', new BMap.Size(20, 35), {
+                imageSize: new BMap.Size(20, 30)
+            })
 
-            // var map = new BMap.Map("allmap");    // 创建Map实例
-            // map.centerAndZoom(new BMap.Point( 118.3,33.96), 10);  // 初始化地图,设置中心点坐标和地图级别
-            // map.addControl(new BMap.MapTypeControl());   //添加地图类型控件
-            // map.setCurrentCity("宿迁");          // 设置地图显示的城市 此项是必须设置的
-            // map.enableScrollWheelZoom(true);  
-            // map.addControl(new BMap.NavigationControl())
-            // $.ajax({
-            //     url: 'check/map',
-            //     dataType: 'json'
-            // })
-            // .done(function(res) {
-            //     console.log("success");
-            //     if (res) {
-            //         console.log(res)
-            //         var list = res['checks'];
-            //         for (var i = 0; i < list.length; i ++) {
-            //             var point = new BMap.Point(list[i].lon, list[i].lat);
-            //             var myIcon = new BMap.Icon("http://223.68.10.47:8888/img/star1.png");
+            var map = new BMap.Map("allmap");    // 创建Map实例
+            map.centerAndZoom(new BMap.Point( 118.3,33.96), 10);  // 初始化地图,设置中心点坐标和地图级别
+            map.addControl(new BMap.MapTypeControl());   //添加地图类型控件
+            map.setCurrentCity("宿迁");          // 设置地图显示的城市 此项是必须设置的
+            map.enableScrollWheelZoom(true);  
+            map.addControl(new BMap.NavigationControl())
+            $.ajax({
+                url: 'check/map',
+                dataType: 'json'
+            })
+            .done(function(res) {
+                console.log("success");
+                if (res) {
+                    console.log(res)
+                    var list = res['checks'];
+                    for (var i = 0; i < list.length; i ++) {
+                        var point = new BMap.Point(list[i].lon, list[i].lat);
+                        var myIcon = new BMap.Icon("http://223.68.10.47:8888/img/star1.png");
 
-            //             var marker;
-            //             var level = list[i].starlevel;
-            //             switch (level) {
-            //                 case '1':
-            //                     marker = new BMap.Marker(point, {icon: icon1});
-            //                     break;
-            //                 case '2':
-            //                     marker = new BMap.Marker(point, {icon: icon2});
-            //                     break;
-            //                 case '3':
-            //                     marker = new BMap.Marker(point, {icon: icon3});
-            //                     break;
-            //                 case '4':
-            //                     marker = new BMap.Marker(point, {icon: icon4});
-            //                     break;
-            //                 case '5':
-            //                     marker = new BMap.Marker(point, {icon: icon5});
-            //                     break;
-            //                 default:
-            //                     marker = new BMap.Marker(point);
-            //             }
-            //             // var marker = new BMap.Marker(point, {icon: icon1}); //
+                        var marker;
+                        var level = list[i].starlevel;
+                        switch (level) {
+                            case '1':
+                                marker = new BMap.Marker(point, {icon: icon1});
+                                break;
+                            case '2':
+                                marker = new BMap.Marker(point, {icon: icon2});
+                                break;
+                            case '3':
+                                marker = new BMap.Marker(point, {icon: icon3});
+                                break;
+                            case '4':
+                                marker = new BMap.Marker(point, {icon: icon4});
+                                break;
+                            case '5':
+                                marker = new BMap.Marker(point, {icon: icon5});
+                                break;
+                            default:
+                                marker = new BMap.Marker(point);
+                        }
+                        // var marker = new BMap.Marker(point, {icon: icon1}); //
 
-            //             marker.info = list[i].name;
-            //             map.addOverlay(marker);
+                        marker.info = list[i].name;
+                        map.addOverlay(marker);
 
-            //             marker.addEventListener('click', function (e) {
-            //                 this.openInfoWindow(new BMap.InfoWindow(this.info));
-            //             })
-            //         }
-            //     }
-            // })
-            // .fail(function(e) {
-            //     console.log(e);
-            // })
-            // .always(function() {
-            //     console.log("complete");
-            // });
+                        marker.addEventListener('click', function (e) {
+                            this.openInfoWindow(new BMap.InfoWindow(this.info));
+                        })
+                    }
+                }
+            })
+            .fail(function(e) {
+                console.log(e);
+            })
+            .always(function() {
+                console.log("complete");
+            });
             
         });
     </script>
