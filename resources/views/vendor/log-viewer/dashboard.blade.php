29afa9340486c5d8b2b5e34fcbd9b7c0e38581f4
amenging <1508235075@qq.com>
2017-11-27 17:02:56 +0800

    reset dashboard file head

--- resources/views/vendor/log-viewer/dashboard.blade.php
+++ resources/views/vendor/log-viewer/dashboard.blade.php
@@ -81,8 +81,137 @@
 </style>
 <div id="allmap" style="width:100%;overflow:hidden;margin:0;z-index:1000"></div>
 <section class="box-body"> {{-- hidden-xs --}}
- 
-<div class="row " style="padding: 20px 0;background: #fff;border-top: 1px solid #cdcdcd;">
+{{-- <ul class="page_ul" style="margin: auto;">
+    <li class="col-sm-6">
+        <a href="check/maptype?type=1" class=" radius-4">
+        <div>
+            <br> <br>
+            <i class="fa fa-fw fa-tv"></i>
+            <br> <br> <br>
+        综合楼可视化
+        </div>
+        </a>
+    </li>
+    <li class="col-sm-6">
+        <a href="check/maptype?type=2" class=" radius-4">
+        <div>
+            <br> <br>
+            <i class="fa fa-fw fa-object-ungroup"></i>
+            <br> <br> <br>
+            营业厅可视化
+        </div>
+        </a>
+    </li>
+    <li class="col-sm-6">
+        <a href="check/maptype?type=3" class=" radius-4">
+        <div>
+            <br> <br>
+            <i class="fa fa-fw fa-object-group"></i>
+            <br> <br> <br>
+            通信基站可视化
+            </div>
+            </a>
+    </li>
+    <li class="col-sm-6">
+        <div>
+        <a href="check/maptype?type=4" class=" radius-4">
+            <br> <br>
+            <i class="fa fa-fw fa-safari"></i>
+            <br> <br> <br>
+            工程建设可视化
+            </a>
+            </div>
+    </li>
+</ul> --}}
+
+{{-- <div class="row">
+    <div class="view_link col-sm-6">
+        <div class="col-sm-10 col-offset-1">
+            综合楼可视化
+        </div>    
+    </div>
+     <div class="view_link col-sm-6">
+        <div class="col-sm-10 col-offset-1">
+            营业厅可视化
+        </div>    
+    </div>
+     <div class="view_link col-sm-6">
+        <div class="col-sm-10 col-offset-1">
+            通信基站可视化
+        </div>    
+    </div>
+     <div class="view_link col-sm-6">
+        <div class="col-sm-10 col-offset-1">
+            工程建设可视化
+        </div>    
+    </div>
+    
+</div> --}}
+<div class="row">
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+              <h3>150</h3>
+
+              <p>综合楼可视化</p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-fw fa-object-group"></i>
+            </div>
+            <a href="check/maptype?type=1" class="small-box-footer">查看 <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-green">
+            <div class="inner">
+              <h3>53</h3>
+
+              <p>营业厅可视化</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a href="check/maptype?type=2" class="small-box-footer">查看 <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-yellow">
+            <div class="inner">
+              <h3>44</h3>
+
+              <p>通信基站可视化</p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-fw fa-tv"></i>
+            </div>
+            <a href="check/maptype?type=3" class="small-box-footer">查看 <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-red">
+            <div class="inner">
+              <h3>65</h3>
+
+              <p>工程建设可视化</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-pie-graph"></i>
+            </div>
+            <a href="check/maptype?type=4" class="small-box-footer">查看 <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+      </div>
+
+</section>
+{{-- <div class="row visible-xs" style="padding: 20px 0;background: #fff;border-top: 1px solid #cdcdcd;">
     <div class="col-xs-6 text-center syinner">
         <a class="index_icon" href="/admin/log-viewer" style="background: #3E9EE0">
                 <i class="ace-icon fa fa-book white" style="font-size: 2em"></i>
@@ -107,13 +236,13 @@
         <div class="icon-text">整改进度</div>
     </div>
     <div class="col-xs-6 text-center syinner">
-        <a class="index_icon" href="/admin/expired/index" style="background: #16C3BC">
+        <a class="index_icon" href="/admin/exposure/index" style="background: #16C3BC">
             <i class="ace-icon fa fa-desktop white" style="font-size: 2em"></i>
             <!-- </div> -->
         </a>
-        <div class="icon-text">预警</div>
+        <div class="icon-text">曝光台</div>
     </div>
-</div> 
+</div> --}}
 @endsection
 
 @section('scripts')
